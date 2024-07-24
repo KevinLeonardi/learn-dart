116e2624bd98fa91fad356ab17f315276a3e7901
Kevin Leonardi <kevinleonardi@yahoo.com>
2024-07-24 07:50:31 +0700

    first commit

---
+++ first_program.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+void main() {
+  // stdout.writeln('What is your name: ?');
+  // var name = stdin.readLineSync();
+  // print('My name is $name');
+
+  print("Hello world");
+
+  int x = 2;
+  var p = 5;
+  dynamic z = 8;
+  z = "cool";
+  // final email = "temid@gmail.com";
+  // final String email = "temid@gmail.com";
+  const qty = 5;
+}
