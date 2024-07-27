a030c3f9c0f994930f0002fc37031ca11dfe4a5c
Kevin Leonardi <kevinleonardi@yahoo.com>
2024-07-27 07:28:10 +0700

    learn variables

--- first_program.dart
+++ first_program.dart
@@ -1,4 +1,4 @@
-import 'dart:io';
+// import 'dart:io';
 
 void main() {
   // stdout.writeln('What is your name: ?');
---
+++ vars.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  //Variables
+
+  //Strings
+  // var name = "John";
+  String name = "John";
+
+  //Integer
+  var x = 41;
+  // x = "Kevin";
+  // int x = 41;
+
+  //Dynamic
+  dynamic firstName = "Tim";
+
+  //Const and Final
+  const String fullName = "John Elder";
+  final String nickName = "J";
+
+  var myName;
+  print(myName);
+
+  myName = "John";
+  print(myName);
+  // print(name);
+  // print(x);
+}
